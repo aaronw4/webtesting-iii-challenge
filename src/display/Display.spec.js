f1e78fd6129b3d81ac53f0e15a7a9f0cdfac4e3d
Aaron Williams <aaronrw4@yahoo.com>
2019-12-04 19:06:55 -0800

    Installed dependencies

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,10 @@
-// Test away!
+import React from 'react';
+import * rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+test('render without crashing', () => {
+   const wrapper = rtl.render(<Dashboard/>);
+   const element = wrapper.getByText(/open/i);
+   expect(element).toBeVisible();
+});
