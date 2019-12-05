ea2e3451c684afca35cb3e03c28abeba74388a9c
Aaron Williams <aaronrw4@yahoo.com>
2019-12-04 21:17:21 -0800

    Added test for Open button class

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -3,8 +3,14 @@ import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './Display';
 
-test('that class is green-led',async () => {
+test('that locked class is green-led',async () => {
    const wrapper = rtl.render(<Display/>);
    const element = wrapper.getByText('Unlocked')
    expect(element).toHaveClass('green-led');
 });
+
+test('that closed class is green-led',async () => {
+    const wrapper = rtl.render(<Display/>);
+    const element = wrapper.getByText('Open')
+    expect(element).toHaveClass('green-led');
+ });
